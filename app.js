cf223ae77ba4b7e16ba53567c20fb39ee0ad1c63
Hamidul Islam shamim <mdhamidul2714@gmail.com>
2022-02-22 19:22:56 +0600

    Finally Done

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const create_devs = document.getElementById('create_devs');
 const skill_list = document.getElementById('skill_list');
+const eskill_list = document.getElementById('eskill_list');
 const add_devs = document.getElementById('add_devs');
 const edit_devs = document.getElementById('edit_devs');
+const clsBtn = document.getElementById('clsBtn');
+const dev_modal_view = document.getElementById('dev_modal_view');
 const devs_Alldata = document.querySelector('.devs_Alldata');
+
 
 
 // skill loads from api
@@ -20,6 +24,22 @@ function devsData() {
 
 }
 
+// eskill loads from api
+eskillData();
+ function eskillData(){
+
+    axios.get('http://localhost:4040/skill').then( eskill=>{
+    let editSkill ='';
+    eskill.data.map(data=>{
+        editSkill += `<option value="${data.id}">${data.name}</option>`
+    });
+
+    eskill_list.insertAdjacentHTML('beforeend', editSkill);
+
+    });
+
+ }
+
 // devs all data
 
 const getDevelopers = () => {
@@ -31,12 +51,12 @@ const getDevelopers = () => {
                             <td> ${index+1} </td>
                             <td>${devs.name}</td>
                             <td colspan="1">${devs.email}</td>
-                           
+                           <td>${skillData(devs.skillId)}</td>
                             <td> <img style="object-fit:cover; width:40px; height: 40px" src="${devs.photo}" alt=""> </td>
                             <td>
-                                <a class="btn btn-info btn-sm" data-bs-toggle ="modal" href="#dev_modal_view"><i class="fa fa-eye"></i> </a>
-                                <a class="btn btn-warning btn-sm" onclick ="editDeveloper(${devs.id})" data-bs-toggle ="modal" href="#dev_modal_edit"><i class="fa fa-edit"></i> </a>
-                                <a class="btn btn-danger btn-sm"  data-bs-toggle ="modal"href="#dev_modal_delete"><i class="fa fa-trash"></i> </a>
+                                <a class="btn btn-info btn-sm text-white" onclick="viewDeveloper(${devs.id})"  data-bs-toggle ="modal" href="#dev_modal_view"><i class="fa fa-eye"></i> </a>
+                                <a class="btn btn-warning btn-sm text-white" onclick ="editDeveloper(${devs.id})" data-bs-toggle ="modal" href="#dev_modal_edit"><i class="fa fa-edit"></i> </a>
+                                <a class="btn btn-danger btn-sm" onclick="deleteDeveloper(${devs.id})" href="#"> <i class="fa fa-trash"></i></a>
                             </td>
                         </tr>`
     });
@@ -100,9 +120,10 @@ axios.get(`http://localhost:4040/developers/${id}`).then( res=> {
     name.value = res.data.name
     email.value = res.data.email
     photo.value = res.data.photo
-    skill.value = res.data.skillId
+    skill.children[res.data.skillId].setAttribute('selected', true)
     preview.setAttribute('src', res.data.photo)
     edit_id.value=id;
+    console.log(res.data.skillId);
 })
 
 }
@@ -117,6 +138,7 @@ e.preventDefault();
      let photo = document.getElementById('ephoto');
      let skill = document.getElementById('eskill_list');
      let edit_id = document.getElementById('edit_id');
+
     axios.patch(`http://localhost:4040/developers/${edit_id.value}`,{
             id: "",
             name: name.value,
@@ -135,3 +157,66 @@ e.preventDefault();
 
     });
 });
+
+
+// this function for delete developers
+
+function deleteDeveloper(id){
+
+    let conf = confirm('Are you sure');
+
+    if(conf==true){
+        axios.delete(`http://localhost:4040/developers/${id}`).then(res => {
+            getDevelopers();
+        })
+    }else{
+        false
+    }
+
+    
+};
+
+
+// view developer data
+ 
+
+function viewDeveloper(id) {
+    let listView = document.querySelector('.listView')
+    
+    axios.get(`http://localhost:4040/developers/${id}`).then( res=>{
+
+        let list_view = '';
+
+        list_view += ` <div class="row">
+                            <div class="col-md-3">
+                                <img style="width: 120px; height: 150px; object-fit: cover;" src="${res.data.photo}"
+                                    alt="">
+                            </div>
+                            <div class="col-md-9">
+                                <div class="card-body">
+                                    <table class="table table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Email</th>
+                                                <th>Skill</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>${res.data.name}</td>
+                                                <td>${res.data.email}</td>
+                                                <td>${skillData(res.data.skillId)}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                      
+                    </div>`
+                    listView.innerHTML = list_view
+    });
+
+    
+}
+
---
+++ function.js
@@ -0,0 +1,13 @@
+function skillData(skillId){
+    if (skillId == "1") {
+        return "Mern Stack"
+    } else if (skillId == "2") {
+        return "PHP Developer"
+    } else if (skillId == "3") {
+        return "Wp Developer"
+    } else if (skillId == "4") {
+        return "Laravel Developer"
+    } else if (skillId == "5") {
+        return "Django Developer"
+    }
+}
